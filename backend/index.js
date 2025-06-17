3c1e51670a01661d3ea48add29eaf42a3256b214
allanmuraroto <allan.muraroto@hotmail.com>
2025-06-17 15:51:10 -0300

    adicionando data, e rotas simples

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,54 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs/promises');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//-------------- arquivo data json ----------------
+
+const caminhoArquivo = path.join(__dirname, 'data.json');
+
+async function lerDados() {
+  const conteudo = await fs.readFile(caminhoArquivo, 'utf-8');
+  return JSON.parse(conteudo);
+}
+
+async function salvarDados(dados) {
+  await fs.writeFile(caminhoArquivo, JSON.stringify(dados, null, 2));
+}
+
+//-------------- fim arquivo data json ----------------
+
 app.use(cors());
 app.use(express.json());
 
-let itens = [];
 let idAtual = 1;
 
-app.get('/item', (req,res) => {
+app.get('/item', async (req,res) => {
+	const itens = await lerDados();
 	res.json(itens);
 });
 
-app.post('/item', (req, res) => {
-	const {nome, imagem} = req.body;
-	if(!nome || !imagem) return res.status(400).json({erro: "nome e imagem obrigatorios"});
+app.post('/item', async (req, res) => {
+	const {nome, imagem, nomeCientifico, origem, descricao} = req.body;
+	if(!nome || !imagem || !nomeCientifico || !origem || !descricao) return res.status(400).json({erro: "nome e imagem obrigatorios"});
 
-	const novoItem = {id : idAtual++, nome, imagem};
+	const novoItem = { id: Date.now(), nome, imagem, nomeCientifico, origem, descricao };
+	const itens = await lerDados(); // garante que vai ler os dados reais do arquivo
 	itens.push(novoItem);
+	await salvarDados(itens);
 	res.status(201).json(novoItem);
-});
+	});
 
-app.delete('/item/:id', (req, res) => {
+app.delete('/item/:id', async (req, res) => {
 	const id = parseInt(req.params.id);
-	item = item.filter(item => item.id !== id);
+	let itens = await lerDados();
+
+	itens = itens.filter(item => item.id !== id);
+	await salvarDados(itens);
 	res.status(204).end();
 });
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ function App() {
   const [item, setItem] = useState([]);
   const [nome, setNome] = useState('');
   const [imagem, setImagem] = useState('');
+  const [nomeCientifico, setNomeCientifico] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [origem, setOrigem] = useState('');
 
   useEffect(() => {
     fetchItem();
@@ -21,12 +24,15 @@ function App() {
     const res = await fetch('http://localhost:3001/item', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ nome, imagem }),
+      body: JSON.stringify({ nome, imagem, nomeCientifico, descricao, origem}),
     });
 
     if (res.ok) {
       setNome('');
       setImagem('');
+      setNomeCientifico('');
+      setDescricao('');
+      setOrigem('');
       fetchItem();
     }
   };
@@ -36,12 +42,22 @@ function App() {
     fetchItem();
   };
 
+  const editarItem = async (id) => {
+    await fetch(`http://localhost:3001/item/${id}`, { method: 'PUT' });
+    fetchItem();
+  };
+
   return (
     <div style={{ padding: 20 }}>
       <h2>Cadastro de Item</h2>
 
       <input placeholder="Nome" value={nome} onChange={e => setNome(e.target.value)} />
       <input placeholder="URL da imagem" value={imagem} onChange={e => setImagem(e.target.value)} />
+      <input placeholder='nome cientifico' value={nomeCientifico} onChange={e => setNomeCientifico(e.target.value)} />
+      <input placeholder='origem/ onde é encontrada' value={origem} onChange={e => setOrigem(e.target.value)} />
+      <input placeholder='Descrição' value={descricao} onChange={e => setDescricao(e.target.value)} />
+
+      
       <button onClick={adicionarItem}>Adicionar</button>
 
       <hr />
@@ -52,7 +68,12 @@ function App() {
           <div key={item.id} style={{ border: '1px solid #ccc', padding: 10, width: 200 }}>
             <img src={item.imagem} alt={item.nome} style={{ width: '100%' }} />
             <h4>{item.nome}</h4>
+            <h4>{item.nomeCientifico}</h4>
+            <h4>{item.origem}</h4>
+            <h4>{item.descricao}</h4>
+            <button style={{ marginRight: '8px' }} onClick={() => editarItem(item.id)}>Editar</button>
             <button onClick={() => removerItem(item.id)}>Remover</button>
+
           </div>
         ))}
       </div>
